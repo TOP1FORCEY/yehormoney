ed600e99f6bbfee6ba570682167f348c42e9e1ad
Eugene Rudenko <romajen2114@gmail.com>
2025-01-23 16:30:51 +0200

    commit

--- tgbot.py
+++ tgbot.py
@@ -1,13 +1,25 @@
-from price import crypto_price
-import requests
+from pytz import timezone
+import requests, time, datetime
 
-from datetime import date
+def crypto_price():
+    url = f"https://api.coingecko.com/api/v3/simple/price?ids=dogecoin&vs_currencies=usd"
+    price = requests.get(url)
+    data = price.json()
+
+    return round((data["dogecoin"]["usd"] - 0.3825) * 2000, 3)
+
+
+dt = datetime.datetime.now(tz = timezone("Europe/Kyiv"))
+now = dt.strftime('%X %A %m/%d/%Y')
+
+translate = {"Monday": "Понеділок", "Tuesday": "Вівторок", "Wednesday": "Середу", "Thursday": "Четверг", "Friday": "П'ятницю", "Saturday": "Субботу", "Sunday": "Неділю",}
+
+for key in translate.keys():
+    now = now.replace(key, translate[key])
 
 BOT_TOKEN = "7795281162:AAEeYa2J23CnT1XwE_JhARdj3ZYisZKUOjQ"
 CHAT_ID = "502712453"  # Use the group chat ID or user chat ID
-MESSAGE = f"Егор станом на {date.today()} має {10} у профіті."
-
-print(crypto_price())
+MESSAGE = f"Егор о {now.split(" ")[0].split(":")[0]}:{now.split(" ")[0].split(":")[1]} у {now.split(" ")[1]} {now.split(" ")[2]} має {crypto_price()} USD у профіті."
 
 def send_message(bot_token, chat_id, message):
     url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
@@ -23,11 +35,11 @@ def send_message(bot_token, chat_id, message):
     else:
         print("Failed to send message. Error:", response.text)
         return False
-    
-    
 
 def main():
-    send_message(BOT_TOKEN, CHAT_ID, MESSAGE)
+    while True:
+        send_message(BOT_TOKEN, CHAT_ID, MESSAGE)
+        time.sleep(21600)
 
 if __name__ == "__main__":
     main()
