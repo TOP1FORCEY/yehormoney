9f55efd10e50730041b6a44c457332df7eab3ddd
Eugene Rudenko <romajen2114@gmail.com>
2025-01-23 19:02:18 +0200

    update

--- tgbot.py
+++ tgbot.py
@@ -1,5 +1,5 @@
 from pytz import timezone
-import requests, time, datetime
+import requests, time, datetime, random
 
 def crypto_price():
     url = f"https://api.coingecko.com/api/v3/simple/price?ids=dogecoin&vs_currencies=usd"
@@ -8,21 +8,26 @@ def crypto_price():
 
     return round((data["dogecoin"]["usd"] - 0.37129) * 2000, 3)
 
-
-dt = datetime.datetime.now(tz = timezone("Europe/Kyiv"))
-now = dt.strftime('%X %A %m/%d/%Y')
+def get_time():
+    dt = datetime.datetime.now(tz = timezone("Europe/Kyiv"))
+    now = dt.strftime('%X %A %m/%d/%Y')
+    for key in translate.keys():
+        now = now.replace(key, translate[key])
+    return now
 
 translate = {"Monday": "Понеділок", "Tuesday": "Вівторок", "Wednesday": "Середу", "Thursday": "Четверг", "Friday": "П'ятницю", "Saturday": "Субботу", "Sunday": "Неділю",}
 
-for key in translate.keys():
-    now = now.replace(key, translate[key])
-
 BOT_TOKEN = "7795281162:AAEeYa2J23CnT1XwE_JhARdj3ZYisZKUOjQ"
 CHAT_ID = "@yehorgainz228"  # Use the group chat ID or user chat ID
-if crypto_price() > 0:
-    MESSAGE = f"Егор о {now.split(" ")[0].split(":")[0]}:{now.split(" ")[0].split(":")[1]} у {now.split(" ")[1]} {now.split(" ")[2]} заробив {crypto_price()} USD."
-else:
-    MESSAGE = f"Егор о {now.split(" ")[0].split(":")[0]}:{now.split(" ")[0].split(":")[1]} у {now.split(" ")[1]} {now.split(" ")[2]} проєбав {crypto_price()} USD."
+
+def initial_masage():
+    
+    now = get_time()
+    
+    if crypto_price() > 0:
+        return f"Егор о {now.split(" ")[0].split(":")[0]}:{now.split(" ")[0].split(":")[1]} у {now.split(" ")[1]} {now.split(" ")[2]} заробив {crypto_price()} USD."
+    else:
+        return f"Егор о {now.split(" ")[0].split(":")[0]}:{now.split(" ")[0].split(":")[1]} у {now.split(" ")[1]} {now.split(" ")[2]} проєбав {crypto_price()} USD."
 
 def send_message(bot_token, chat_id, message):
     url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
@@ -33,7 +38,7 @@ def send_message(bot_token, chat_id, message):
     response = requests.post(url, json=payload)
     
     if response.status_code == 200:
-        print(f"Message ({MESSAGE}) sent successfully!")
+        print(f"Message ({message}) sent successfully!")
         return True
     else:
         print("Failed to send message. Error:", response.text)
@@ -41,8 +46,8 @@ def send_message(bot_token, chat_id, message):
 
 def main():
     while True:
-        send_message(BOT_TOKEN, CHAT_ID, MESSAGE)
-        time.sleep(600)
+        send_message(BOT_TOKEN, CHAT_ID, initial_masage())
+        time.sleep(random.randint(500, 1000))
 
 if __name__ == "__main__":
     main()
